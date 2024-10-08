d3f42ad49bd4bf7c18667b44476ff747d3921690
Crystal Zhang <crystalz16@nycstudents.net>
2024-10-08 17:25:54 -0400

    yay

---
+++ Zhang_Crystal_Lab_5_Part3_Part1.cpp
@@ -0,0 +1,46 @@
+//Crystal Zhang 
+//10/08/2024 
+//Lab 5 Part 3 
+
+#include <iostream> 
+#include <ctime>
+#include <cstdlib.>
+
+using namespace std; 
+
+int main () 
+{ 
+    int direct;
+    int north = 0;
+    int south = 0; 
+    int east = 0; 
+    int west = 0;
+    int steps = 0; 
+
+    srand(time(0));
+    
+    for (int i=1; i<=25; i++)
+    {
+        direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
+        //1 means North, 2 means South, 3 means East and 4 means West
+
+        if (direct == 1) 
+            north = north + 1;
+
+        else if (direct == 2) 
+            south = south + 1; 
+
+        else if (direct == 3) 
+            east = east + 1; 
+
+        else
+            west = west + 1;
+    }
+
+    cout << "The robot is " << north 
+    << " blocks north from the starting point and " << south 
+    << " blocks south from the starting point " << east 
+    << " blocks east for the starting point " << west 
+    << " blocks west from the starting point." << endl; 
+}
+
--- Zhang_Crystal_Lab_5_Part3_Part2.cpp
+++ Zhang_Crystal_Lab_5_Part3_Part2.cpp
@@ -1,6 +1,6 @@
 //Crystal Zhang 
 //10/08/2024 
-//Lab 5 Part 3 
+//Lab 5 Part 3 Part 2
 
 #include <iostream> 
 #include <ctime>
@@ -18,34 +18,6 @@ int main ()
     int steps = 0; 
 
     srand(time(0));
-    
-    /*
-    for (int i=1; i<=25; i++)
-    {
-        direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
-        //1 means North, 2 means South, 3 means East and 4 means West
-
-        if (direct == 1) 
-            north = north + 1;
-
-        else if (direct == 2) 
-            south = south + 1; 
-
-        else if (direct == 3) 
-            east = east + 1; 
-
-        else
-            west = west + 1;
-    }
-
-    cout << "The robot is " << north 
-    << " blocks north from the starting point and " << south 
-    << " blocks south from the starting point " << east 
-    << " blocks east for the starting point " << west 
-    << " blocks west from the starting point." << endl; 
-    */
-
-
       while ((east <= 2) && (north <= 3))
     {
         direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
