76fba3c31a8b6b76ade060857e68b85577517184
Crystal Zhang <crystalz16@nycstudents.net>
2024-10-08 17:48:26 -0400

    xv

--- Zhang_Crystal_Lab_5_Part3_Part2.cpp
+++ Zhang_Crystal_Lab_5_Part3_Part2.cpp
@@ -48,6 +48,6 @@ int main ()
             }
     }
    
-    cout << "It took the robot " << steps << " steps to move two blocks east and three blocks north" << endl; 
+    cout << "It took the robot " << steps << " steps to move two blocks east and three blocks north" << north << east << endl; 
     return 0;
 }
