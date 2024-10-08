006110babaaf0f19b2a7309c0b160b39741659ae
Crystal Zhang <crystalz16@nycstudents.net>
2024-10-08 17:07:47 -0400

    Zhang_Crystal_Lab_5

--- Zhang_Crystal_Lab_5_Part3.cpp
+++ Zhang_Crystal_Lab_5_Part3.cpp
@@ -3,39 +3,38 @@
 //Lab 5 Part 3 
 
 #include <iostream> 
-
+#include <ctime>
+#include <cstdlib.>
 
 using namespace std; 
 
 int main () 
 { 
-     cout << rand() << ", " << rand() << endl;
-    /*
-    srand(time(0));
     int direct;
-    int i;
-    int north=0;
-    int south=0; 
-    int east=0; 
-    int west=0;
+    int north  = 0;
+    int south = 0; 
+    int east = 0; 
+    int west = 0;
+    int steps = 0; 
 
-    direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
-    //1 means North, 2 means South, 3 means East and 4 means West
-
-    cout << direct << endl;
-
-     for (int i; i<=5; i++)
+    srand(time(0));
+    
+    /*
+     for (int i=1; i<=25; i++)
     {
-        if (direct = 1) 
+        direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
+        //1 means North, 2 means South, 3 means East and 4 means West
+
+        if (direct == 1) 
             north = north + 1;
 
-        if (direct = 2) 
+        else if (direct == 2) 
             south = south + 1; 
 
-        if (direct = 3) 
+        else if (direct == 3) 
             east = east + 1; 
 
-        if (direct = 4) 
+        else
             west = west + 1;
     }
 
@@ -45,6 +44,25 @@ int main ()
     << " blocks east for the starting point " << west 
     << " blocks west from the starting point." << endl;
     */
+
+      while (east <= 2 && north <= 3)
+    {
+        direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
+        //1 means North, 2 means South, 3 means East and 4 means West
+
+        if (direct == 1) 
+            north = north + 1;
+
+        else if (direct == 2) 
+            south = south + 1; 
+
+        else if (direct == 3) 
+            east = east + 1; 
+
+        else
+            west = west + 1;
+    }
    
+    cout << "It took the robot " << steps << " steps to move two blocks east and three blocks north" << endl; 
     return 0;
 }
